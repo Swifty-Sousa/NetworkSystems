2e056b8cba0f0c848ab1f5dafb30e6ac5b252df7
Swifty-Sousa <chso8299@colorado.edu>
2020-12-08 18:23:13 -0700

    added starter code to try on elra

---
+++ pa4/df_client.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <netdb.h>
+#include <dirent.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <openssl/md5.h>
+
+#define MAXLINE 8192
+#define BUF 256
+#define MAXFILES 128
+#define MAXFILENAME 64
+
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
